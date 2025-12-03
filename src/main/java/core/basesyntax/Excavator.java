f3472e1851926a4434d94e3a156f2e908f72824b
kastashenkova <astashenkova.katya@gmail.com>
2025-12-04 00:06:44 +0200

    created polymorphism

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class Excavator extends Machine {
     @Override
     public void doWork() {
-        System.out.println("Pr-pr-pr-pr");
+        System.out.println("Excavator started its work.");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Pra-a-a");
+        System.out.println("Excavator stopped its work.");
     }
 }
